e45035a89917a033d1eb027e363895b7009f9d80
miztch <miztch.me@gmail.com>
2023-05-09 23:28:02 +0900

    exercise 008 add another solution

--- exercises/008.sh
+++ exercises/008.sh
@@ -1,1 +1,4 @@
 cat ../qdata/8/access.log | awk '{print $4}' | sed -e 's|\/| |g' | sed -e 's|:| |g' | awk '{if ($4 <= 12) print "am"; else print "pm"}' | sort | uniq -c
+
+# sed -e は1コマンドでいける
+# cat ../qdata/8/access.log | awk '{print $4}' | sed -e 's|\/| |g' -e 's|:| |g' | awk '{if ($4 <= 12) print "am"; else print "pm"}' | sort | uniq -c
